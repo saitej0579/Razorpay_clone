220069288dbd8c58d18609b599abc4afcb0c5334
sai teja <yanagandulasaiteja0579@gmail.com>
2024-05-23 11:37:41 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["mullish","sans-serif"],
+      },
+      colors:{
+        deepblue: "#02042a",
+        lightblue: " #4593ed",
+        lightblue300: "#4b94ed",
+        lightblue500: "#0b74e7",
+        greenlight: "#61cea6",
+        greytext: "#818597",
+        lightgray: "#e2e2e2",
+        grayblue: "#344a6c",
+        deepbluehead: "#162f56",
+        gray2: "#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
